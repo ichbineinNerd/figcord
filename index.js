3c4ae13240d17e3ba568134b2d9c28090ebdafb6
Tilman B <groessternerdallerzeiten@web.de>
2019-08-06 23:15:05 +0200

    node.js is trolling me *cries in nerd*

--- index.js
+++ index.js
@@ -193,13 +193,13 @@ const parseFigFont = function parseFigFont(data) {
             }
 
             if (charLine === height - 1) {
-                thisChar += l.replace(hardBlank, ' ');
+                thisChar += l.replace(new RegExp(hardBlank), ' ');
                 if (thisChar.endsWith(ending + ending))
                     thisChar = thisChar.substr(0, thisChar.length - 2);
                 else
                     thisChar = thisChar.substr(0, thisChar.length - 1);
             }else {
-                thisChar += l.replace(hardBlank, ' ').substr(0, l.length - 1) + '\n';
+                thisChar += l.replace(new RegExp(hardBlank), ' ').substr(0, l.length - 1) + '\n';
             }
             lines = lines.slice(1);
         }
